ff8bee324dc5e19a37f3b64243172e7f55c9a480
Chfer <183066043+chfer-sys@users.noreply.github.com>
2026-07-13 02:25:58 +1000

    chore: untrack config.js (per SPEC)

--- config.js
+++
@@ -1,3 +0,0 @@
-// MiniChat Translator - Configuration
-const apiKey = 'sk-cp-JRVfuT5Rd0z_A1SJMBJh5iqf4tiOmvjg3l2ejn2xTrryUYv69mBBM8siOhh6zH6HNOMvr2GbFAIhPkKV1kCzlK_P309yXe4DeGd4ADiSzt9dFbF1k_OcmLk';
-const baseUrl = 'https://api.minimax.io/v1';
